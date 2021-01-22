eb4ef6faef659fb1d6509b80ae0a5d9b5932c1e1
imcarissa <carissa.sarreal@gmail.com>
2021-01-22 12:01:19 -0500

    new controller file added

---
+++ app/controllers/wine_entries_controller.rb
@@ -0,0 +1,17 @@
+class WineEntriesController < ApplicationController
+
+    # get wine_entries/new to render a form create a new entry
+    get '/wine_entries/new' do
+        erb :'/wine_entries/new'
+    end
+
+    # post wine_entries to create a new wine entry
+
+
+    # show a route for a wine entry
+
+
+    # index route for all wine entries
+
+
+end
