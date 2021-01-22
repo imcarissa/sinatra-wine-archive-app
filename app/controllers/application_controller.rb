a65e9d553e7b2a854edf474e24fb5641b510891f
imcarissa <carissa.sarreal@gmail.com>
2021-01-22 05:41:43 -0500

    enable sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,14 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "we_were_on_a_break"
   end
 
   get "/" do
     erb :welcome
   end
 
-  
+
 
 end
