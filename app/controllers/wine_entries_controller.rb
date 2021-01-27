38c5f2b3779993e2593c886f2b387ea6cf5eb1f1
imcarissa <carissa.sarreal@gmail.com>
2021-01-27 03:52:34 -0500

    create a post route to wine_entries

--- app/controllers/wine_entries_controller.rb
+++ app/controllers/wine_entries_controller.rb
@@ -7,11 +7,23 @@ class WineEntriesController < ApplicationController
 
     # post wine_entries to create a new wine entry
     post '/wine_entries' do
-        
+        # I want to create a new wine entry and save it to the DB
+        # I only want to save the entry if it has content
+        if !logged_in?
+            redirect '/'
+        end
+        # I want to create a wine entry if the user is logged in
+        if params[:type] != ""
+            # create a new entry
+            @wine_entry = WineEntry.create(type: params[:type], user_id: current_user.id)
+            redirect "/wine_entries/#{@wine_entry.id}"
+        else
+            redirect '/wine_entries/new'
+        end
     end
 
     # show a route for a wine entry
-
+    
 
     # index route for all wine entries
 
