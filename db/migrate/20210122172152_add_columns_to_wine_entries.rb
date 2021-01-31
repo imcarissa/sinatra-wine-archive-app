13585d61301125847427ca1410e2c11a00aceb78
imcarissa <carissa.sarreal@gmail.com>
2021-01-31 18:18:37 -0500

    deleted files

--- db/migrate/20210122172152_add_columns_to_wine_entries.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToWineEntries < ActiveRecord::Migration
-  def change
-    add_column :wine_entries, :price, :integer
-    add_column :wine_entries, :region, :string
-  end
-end
--- db/migrate/20210131221240_create_wine_entries_table.rb
+++ db/migrate/20210131221240_create_wine_entries_table.rb
@@ -3,9 +3,6 @@ class CreateWineEntries < ActiveRecord::Migration
     create_table :wine_entries do |t|
       t.string :wine_type
       t.integer :user_id
-      t.string :region
-      t.integer :vintage
-      t.integer :price
       t.timestamps null: false
     end
   end
