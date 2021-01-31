1bf37cee9e15a4f4ffe8b5fd43aea3adeda94fa2
imcarissa <carissa.sarreal@gmail.com>
2021-01-31 18:33:09 -0500

    created new seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,17 +3,17 @@
 # Create 2 users
 
 rachel = User.create(name: "Rachel", email: "RMGreen@bloomingdales.com", password: "password1234")
-monica = User.create(name: "Monica", email: "ChefGellarM@Javu.net", password: "7ways2clean")
+monica = User.create(name: "Monica", email: "ChefGellar@Javu.net", password: "7ways2clean")
 
 # Create some wine entries
 
-WineEntry.create(user_id: rachel.id , vintage: 1999)
+WineEntry.create(wine_type: "merlot", user_id: rachel.id)
         # make new seed file matching schema
         # where there is 'type'
 
 
 # Use AR to pre-associate data
 
-rachel.wine_entries.create(vintage: 2005)
-monica_entry = monica.wine_entries.build(vintage: 2010)
+rachel.wine_entries.create(wine_type: "sauvignon blanc")
+monica_entry = monica.wine_entries.build(wine_type: "merlot")
 monica_entry.save
