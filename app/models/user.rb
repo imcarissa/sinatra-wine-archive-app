da73bfd76dcf94d04c4ee0020a12ef75854fe2ee
imcarissa <carissa.sarreal@gmail.com>
2021-01-31 14:23:18 -0500

    fix syntax errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
     has_secure_password
-    validates :name, :presence: true
+    validates :name, presence: true
     validates :email, presence: true
     validates :email, uniqueness: true
 
     has_many :wine_entries
+
 end
