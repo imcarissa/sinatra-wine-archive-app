ecddb46574f4a85515ba5246e0c267c6ee7602e5
imcarissa <carissa.sarreal@gmail.com>
2021-02-01 17:30:32 -0500

    change to database

---
+++ db/migrate/20210201222546_add_columns_to_wine_entries_table.rb
@@ -0,0 +1,8 @@
+class AddColumnsToWineEntriesTable < ActiveRecord::Migration
+  def change
+    add_column :wine_entries, :wine_name, :string, {:default=>nil}
+          add_column :wine_entries, :vintage, :integer, {:default=>nil}
+          add_column :wine_entries, :region, :string, {:default=>nil}
+          add_column :wine_entries, :wine_notes, :string, {:default=>nil}
+  end
+end
