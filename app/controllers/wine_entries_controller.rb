a55f8bf7d166965454341c86f286e54c76f69f43
imcarissa <carissa.sarreal@gmail.com>
2021-02-01 21:10:17 -0500

    found typo in code and fixed it

--- app/controllers/wine_entries_controller.rb
+++ app/controllers/wine_entries_controller.rb
@@ -15,8 +15,8 @@ class WineEntriesController < ApplicationController
     
     post '/wine_entries' do
         redirect_if_not_logged_in
-        if params[:wine_name] != "" && params[:wine_type] != "" && params[:vintage] != "" && params[:region] != "" && params[:wine_notes] != ""
-            @wine_entry = WineEntry.create(wine_type: params[:wine_type], user_id: current_user.id, wine_name: params[:wine_name], vintage: params[:vintage], region: params[:region], wine_notes: params[:wine_notes])
+        if params[:wine_name] !=""
+            @wine_entry = WineEntry.create(wine_type: params[:type], user_id: current_user.id, wine_name: params[:name], vintage: params[:vintage], region: params[:region], wine_notes: params[:wine_notes])
             flash[:message] = "Wine entry successfully created" if @wine_entry.id
             redirect "/wine_entries/#{@wine_entry.id}"
         else
@@ -46,8 +46,8 @@ class WineEntriesController < ApplicationController
     patch '/wine_entries/:id/' do
         redirect_if_not_logged_in
         set_wine_entry
-        if authorized_to_edit?(wine_entry) && params[:wine_type] != ""
-            @wine_entry.update(wine_type: params[:wine_type])
+        if authorized_to_edit?(wine_entry) && params[:wine_name] != ""
+            @wine_entry.update(wine_type: params[:wine_type], user_id: current_user.id, wine_name: params[:wine_name], vintage: params[:vintage], region: params[:region], wine_notes: params[:wine_notes])
             redirect "/wine_entries/#{@wine_entry.id}"
         else
             redirect "users/#{current_id}"
@@ -69,7 +69,7 @@ class WineEntriesController < ApplicationController
     private
     
     def set_wine_entry
-        @wine_entry = WineEntry.find(params[:id])
+        @wine_entry = WineEntry.find_by(id: params[:id])
     end
     
 
