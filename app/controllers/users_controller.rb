eb702aa1dac5dc54d2bae3694c936856bc350880
imcarissa <carissa.sarreal@gmail.com>
2021-02-06 20:49:28 -0500

    fied a typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 
     post '/login' do
         @user = User.find_by(email: params[:email])
-        if@user &&  @user.authenticate(params[:password])
+        if @user &&  @user.authenticate(params[:password])
             session[:user_id] = @user.id
             flash[:message] = "Welcome, #{@user.name}!"
             redirect "/users/#{@user.id}"
