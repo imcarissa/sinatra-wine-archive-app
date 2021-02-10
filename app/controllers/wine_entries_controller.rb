49be2dedad5bd906fbfe48bfbbab8f5ef53a42b6
imcarissa <carissa.sarreal@gmail.com>
2021-02-09 23:17:31 -0500

    all fields must have content to be able to create instance of wine entry

--- app/controllers/wine_entries_controller.rb
+++ app/controllers/wine_entries_controller.rb
@@ -5,8 +5,8 @@ class WineEntriesController < ApplicationController
         @wine_entries = WineEntry.all
         erb :'/wine_entries/index'
     end
-    
-    
+  
+      
     get '/wine_entries/new' do
         redirect_if_not_logged_in
         erb :'/wine_entries/new'
@@ -15,12 +15,12 @@ class WineEntriesController < ApplicationController
     
     post '/wine_entries' do
         redirect_if_not_logged_in
-        @wine_entry = WineEntry.create(wine_type: params[:wine_type], user_id: current_user.id, wine_name: params[:wine_name], vintage: params[:vintage], region: params[:region], wine_notes: params[:wine_notes])
-        if @wine_entry.valid? && params[:wine_type] != ""
+        if params[:wine_name] !=""
+            @wine_entry = WineEntry.create(wine_type: params[:wine_type], user_id: current_user.id, wine_name: params[:wine_name], vintage: params[:vintage], region: params[:region], wine_notes: params[:wine_notes])
             flash[:message] = "Wine entry successfully created" if @wine_entry.id
             redirect "/wine_entries/#{@wine_entry.id}"
         else
-            flash[:errors] = "Uh oh! You can't have a blank entry. Please provide content for at least one of the spaces provided. You can edit the entry later."
+            flash[:errors] = "Uh oh! Please provide some content for all the spaces provided. You can edit the entry later."
             redirect '/wine_entries/new'
         end
     end
@@ -46,7 +46,7 @@ class WineEntriesController < ApplicationController
     patch '/wine_entries/:id' do
         redirect_if_not_logged_in
         set_wine_entry
-        if authorized_to_edit?(@wine_entry) && params[:wine_type] != ""
+        if authorized_to_edit?(@wine_entry) && params[:wine_name] != ""
             @wine_entry.update(wine_type: params[:wine_type], user_id: current_user.id, wine_name: params[:wine_name], vintage: params[:vintage], region: params[:region], wine_notes: params[:wine_notes])
             flash[:message] = "Successfully saved!"
             redirect "/wine_entries/#{@wine_entry.id}"
@@ -54,6 +54,7 @@ class WineEntriesController < ApplicationController
             redirect "/users/#{current_user.id}"
         end
     end
+
 
     delete '/wine_entries/:id' do
         set_wine_entry
